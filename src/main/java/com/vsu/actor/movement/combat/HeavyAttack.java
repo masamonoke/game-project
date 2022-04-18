27c470b3a1a06f2bcb1ebe81e03b9f33d1b8ad0a
masamonoke <m.rudakov@yahoo.com>
2022-04-18 17:56:52 +0300

    Небольшая реорганизация классов, начало работы над боевой системой

---
+++ src/main/java/com/vsu/actor/movement/combat/HeavyAttack.java
@@ -0,0 +1,10 @@
+package com.vsu.actor.movement.combat;
+
+import com.vsu.actor.movement.Movement;
+
+public class HeavyAttack extends Movement {
+    @Override
+    public Movement move(Movement prev) {
+        return null;
+    }
+}
---
+++ src/main/java/com/vsu/actor/movement/combat/LightAttack.java
@@ -0,0 +1,10 @@
+package com.vsu.actor.movement.combat;
+
+import com.vsu.actor.movement.Movement;
+
+public class LightAttack extends Movement {
+    @Override
+    public Movement move(Movement prev) {
+        return null;
+    }
+}
