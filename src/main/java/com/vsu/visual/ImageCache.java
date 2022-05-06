8e4e37bd7edbbcc1f12195698d1c4515854cb5c1
Станислав <90761337+GreatAngel01@users.noreply.github.com>
2022-05-06 20:03:30 +0300

    Lol

--- src/main/java/com/vsu/visual/ImageCache.java
+++ src/main/java/com/vsu/visual/ImageCache.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class ImageCache {
     private final Map<String, Image> imageMap;
-
+ //Ghjd
     public ImageCache() {
         this.imageMap = new HashMap<>();
     }
