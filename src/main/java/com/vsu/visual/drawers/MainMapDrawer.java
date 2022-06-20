a19aed329d17d4f542ecfc6a86f41ee2ee5d8f6d
Станислав <90761337+GreatAngel01@users.noreply.github.com>
2022-06-20 17:22:47 +0300

    изменение отрисовки стен

--- src/main/java/com/vsu/visual/drawers/MainMapDrawer.java
+++ src/main/java/com/vsu/visual/drawers/MainMapDrawer.java
@@ -13,15 +13,15 @@ public class MainMapDrawer extends Drawer {
 
     @Override
     public void draw() {
-      //  data.getMainMapCanvas().getGraphicsContext2D().setFill(Color.BLACK);
-     //   data.getMainMapCanvas().getGraphicsContext2D().fillRect(0, 0,
-      //          data.getMainMapCanvas().getWidth(), data.getMainMapCanvas().getHeight());
-    //    data.getMainMapCanvas().getGraphicsContext2D().fill();
-        data.getMainMapCanvas().getGraphicsContext2D().drawImage(ViewConfig.getInstance().getImageCache().
-                getImageByPath("/img/underground/backgroundDungeon.jpg"),0,0);
+        data.getMainMapCanvas().getGraphicsContext2D().setFill(Color.BLACK);
+        data.getMainMapCanvas().getGraphicsContext2D().fillRect(0, 0,
+                data.getMainMapCanvas().getWidth(), data.getMainMapCanvas().getHeight());
+        data.getMainMapCanvas().getGraphicsContext2D().fill();
+
 
         for (int i = 1; i < data.getTilemap().getRowSize() - 2; i++) {
             for (int j = 1; j < data.getTilemap().getColSize() - 2; j++) {
+
                 if (data.getTilemap().getMatrix()[i][j].getType() != TileType.Wall) {
                     drawWalls(i, j);
                 }
@@ -31,11 +31,10 @@ public class MainMapDrawer extends Drawer {
     }
 
     private void drawWalls(int row, int col) {
-        for (int i = row - 1; i < row +2; i++) {
+        for (int i = row - 1; i < row + 2; i++) {
             for (int j = col - 1; j < col + 2; j++) {
                 Image image = ViewConfig.getInstance().getTileTypeImageMap().get(data.getTilemap().getMatrix()[i][j].getType());
-                data.getMainMapCanvas().getGraphicsContext2D()
-                        .drawImage(image, j * data.getTileSize(), i * data.getTileSize());
+                data.getMainMapCanvas().getGraphicsContext2D().drawImage(image, j * data.getTileSize(), i * data.getTileSize());
             }
         }
     }
